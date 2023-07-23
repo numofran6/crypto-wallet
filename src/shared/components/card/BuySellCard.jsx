bb062c02bc7e177f17fbe3b06639a0fff79c0a8a
Francis <numofran6@gmail.com>
2023-07-23 11:03:43 +0000

    card shadow adjusted & buy/sell buttons gradient implemented

--- src/shared/components/card/BuySellCard.jsx
+++ src/shared/components/card/BuySellCard.jsx
@@ -1,17 +1,21 @@
-import { BiSolidDollarCircle } from 'react-icons/bi';
 import './BuySellCard.scss';
+import { FaDollarSign } from 'react-icons/fa';
 
 export const BuySellCard = ({ symbol }) => {
 	return (
 		<div className="buysellcard-container">
 			<button className="buysellcard card-shadow">
-				<BiSolidDollarCircle color="#1aa0f7" className="buysellcard-currency" />
+				<div className="buysellcard-currency buy-color">
+					<FaDollarSign className="w-5 h-5" />
+				</div>
 
 				<h2 className="buysellcard-title">Buy {symbol}</h2>
 			</button>
 
 			<button className="buysellcard card-shadow">
-				<BiSolidDollarCircle color="#fc3b95" className="buysellcard-currency" />
+				<div className="buysellcard-currency sell-color">
+					<FaDollarSign className="w-5 h-5" />
+				</div>
 
 				<h2 className="buysellcard-title">Sell {symbol}</h2>
 			</button>
